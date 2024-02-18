feda124ba4b65c83f1f6668258a76d15878d8236
Rodrigo <rodrigo.martinez@webexport.com.ar>
2024-02-18 20:27:18 +0000

    fix / strapi instance api url request

--- src/lib/strapi.js
+++ src/lib/strapi.js
@@ -1,6 +1,6 @@
 import Strapi from "strapi-sdk-js";
 
-const strapi = new Strapi({url: process.env.API_URL, store: {
+const strapi = new Strapi({url: process.env.NEXT_PUBLIC_API_URL, store: {
   key: "strapi_jwt"
 }});
 
